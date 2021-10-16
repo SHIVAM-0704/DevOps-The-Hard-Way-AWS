b1dab087316bfad1c74e02343c7d22e0b66f6451
Shivam Sharma <shivam7733sharma@gmail.com>
2021-10-16 19:48:31 +0530

    Update variables.tf

--- Terraform-AWS-Services-Creation/EKS-With-Worker-Nodes/variables.tf
+++ Terraform-AWS-Services-Creation/EKS-With-Worker-Nodes/variables.tf
@@ -1,10 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-07d002bb7e32b67fc"
+  default = "subnet-0cca2923c707ed7c2"
 }
 
 variable "subnet_id_2" {
   type = string
-  default = "subnet-03c49bbe3a085ef44"
+  default = "subnet-055ba68fa8e0ad0a2"
 }
-
