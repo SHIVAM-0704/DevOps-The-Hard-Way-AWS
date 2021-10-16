d4c3d2f678cfd2c4d03b83fdbd28d26054e39b38
Shivam Sharma <shivam7733sharma@gmail.com>
2021-10-16 19:46:15 +0530

    Update main.tf

--- Terraform-AWS-Services-Creation/terraform-state-s3-bucket/main.tf
+++ Terraform-AWS-Services-Creation/terraform-state-s3-bucket/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-devopsthehardway"
+  bucket = "terraform-state-devopsthehardway0702"
   versioning {
     enabled = true
   }
