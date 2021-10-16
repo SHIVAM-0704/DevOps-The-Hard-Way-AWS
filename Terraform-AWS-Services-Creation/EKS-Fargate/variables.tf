80d9be2438adb034487b0e2bec56b5fdca1b38e5
Shivam Sharma <shivam7733sharma@gmail.com>
2021-10-16 19:50:15 +0530

    Update variables.tf

--- Terraform-AWS-Services-Creation/EKS-Fargate/variables.tf
+++ Terraform-AWS-Services-Creation/EKS-Fargate/variables.tf
@@ -1,14 +1,15 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-0724276a66ddfe51e"
+  default = "subnet-0cca2923c707ed7c2"
 }
 
 variable "subnet_id_2" {
   type = string
-  default = "subnet-0bc007da6e373a517"
+  default = "subnet-055ba68fa8e0ad0a2"
 }
 
 variable "private_subnet_id_1" {
   type = string
-  default = "subnet-0a18be575c2cd0968"
+  default = "subnet-0f36dbec637df9420"
 }
+
